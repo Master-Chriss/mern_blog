3a00414186904d1d222056380c2bb036ab277c3a
Master_Chriss <masterchriss001@gmail.com>
2026-04-12 05:26:37 +0200

    added host print message

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -6,8 +6,9 @@ const dbUrl = process.env.DB_URL;
 
 const connectToDB = async (dbUrl) => {
 	try {
-		await mongoose.connect(dbUrl);
+		const conn = await mongoose.connect(dbUrl);
 		console.log('Server successfully connected to DB 😂😸😂');
+		console.log(conn.connection.host, 'DB Host');
 	} catch (error) {
 		console.log(error, 'Server Failed to connect ⚠👀⚠');
 	}
